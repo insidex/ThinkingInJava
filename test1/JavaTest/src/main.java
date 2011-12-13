8fb54713f3e00db3a04ec77c6784779fb37bf15c
илья <@boyarin.prometeyspb.local>
2011-12-13 12:53:02 +0400

    Signed-off-by: илья <илья@boyarin.prometeyspb.local>

--- test1/JavaTest/src/main.java
+++ test1/JavaTest/src/main.java
@@ -14,7 +14,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello man!");
+		System.out.println("Buy world!");
 
 	}
 
