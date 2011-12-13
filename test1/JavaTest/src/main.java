58f1a5f2a52db9b4e60fcd2e222829ea4475807b
илья <@boyarin.prometeyspb.local>
2011-12-13 12:50:51 +0400

    Signed-off-by: илья <илья@boyarin.prometeyspb.local>

---
+++ test1/JavaTest/src/main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author илья
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
